eedca70e4233b26f865e517cd83a4c61cfad0248
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:11:43 +0300

    Add project files.

---
+++ Max_min/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введіть три числа:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = Math.Max(a, Math.Max(b, c));
+int min = Math.Min(a, Math.Min(b, c));
+
+Console.WriteLine($"Max: {max}, Min: {min}");
